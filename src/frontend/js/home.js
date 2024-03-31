d57013f592b3dd37a3c81205c34f11af9420925d
ryhara <ryosei.hara@gmail.com>
2024-03-31 16:19:45 +0900

    [Add] ゲーム開始画面の表面作成

---
+++ src/frontend/js/home.js
@@ -0,0 +1,8 @@
+
+document.getElementById('startButton').addEventListener('click', () => {
+	window.location.href = 'start.html';
+});
+
+document.getElementById('dashboardButton').addEventListener('click', () => {
+	window.location.href = 'dashboard.html';
+});

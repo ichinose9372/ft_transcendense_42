dfa78b2ab3132060c4cd8c00b01785b8925921a5
yichinos <ichinose.0720.emt@gmail.com>
2024-04-05 17:40:29 +0900

    add: settingfiles #12

--- src/backend/transcendence/transcendence/settings.py
+++ src/backend/transcendence/transcendence/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'transcendence.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'transcendence_db',
+        'USER': 'user42',
+        'PASSWORD': 'user42',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -103,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 

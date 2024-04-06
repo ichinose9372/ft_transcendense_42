aa00e5709baa8e57ddaee468a593f4ba176d6620
yichinos <ichinose.0720.emt@gmail.com>
2024-04-07 08:42:38 +0900

    fix : dashbord, dashboard #12

--- src/backend/transcendence/dashboard/apps.py
+++ src/backend/transcendence/dashboard/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DashbordConfig(AppConfig):
+class DashboardConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dashbord'
+    name = 'dashboard'

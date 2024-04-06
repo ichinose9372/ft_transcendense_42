b8c1019bbe0f6ad091f12fe7fb1564ac606a7615
snemoto-42 <115632171+snemoto-42@users.noreply.github.com>
2024-04-06 15:24:21 +0900

    #14 veiw関数の追加

--- src/backend/transcendence/game/tests.py
+++ src/backend/transcendence/game/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 
 from django.http import HttpRequest
-from game.views import top
+from django.urls import resolve
+from game.views import top, game_start, game_finish
 
 # Create your tests here.
 class TopPageViewTest(TestCase):
@@ -12,3 +13,13 @@ class TopPageViewTest(TestCase):
 	def test_top_returns_expected_content(self):
 		response = self.client.get("/")
 		self.assertEqual(response.content, b"Hello Transcendence")
+
+class GameStartTest(TestCase):
+	def test_should_resolve_game_start(self):
+		found = resolve("/game/start/")
+		self.assertEqual(game_start, found.func)
+
+class GameFinishTest(TestCase):
+	def test_should_resolve_game_finish(self):
+		found = resolve("/game/finish/")
+		self.assertEqual(game_finish, found.func)
--- src/backend/transcendence/transcendence/urls.py
+++ src/backend/transcendence/transcendence/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from game.views import top
 
@@ -26,4 +26,5 @@ from game.views import top
 urlpatterns = [
     path('admin/', admin.site.urls),
 	path('', top, name="top"),
+	path('game/', include('game.urls')), # game/urls.py の読み込み
 ]

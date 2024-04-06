5b45d6ce57ebae534f203923063080c0d1104ad5
snemoto-42 <115632171+snemoto-42@users.noreply.github.com>
2024-04-06 14:57:10 +0900

    #14_game

--- src/backend/transcendence/game/tests.py
+++ src/backend/transcendence/game/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
+from django.http import HttpRequest
+from game.views import top
+
 # Create your tests here.
+class TopPageViewTest(TestCase):
+	def test_top_returns_200(self):
+		request = HttpRequest()
+		response = top(request)
+		self.assertEqual(response.status_code, 200)
+	
+	def test_top_returns_expected_content(self):
+		request = HttpRequest()
+		response = top(request)
+		self.assertEqual(response.content, b"Hello Transcendence")

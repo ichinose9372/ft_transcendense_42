6c0401c39926897dd6d9ae72945ce8790f2c03d1
snemoto-42 <115632171+snemoto-42@users.noreply.github.com>
2024-04-06 19:48:28 +0900

    #17 for marge

--- src/backend/transcendence/transcendence/settings.py
+++ src/backend/transcendence/transcendence/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+# manage.pyがあるディレクトリを指す
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -76,24 +77,24 @@ WSGI_APPLICATION = 'transcendence.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'transcendence_db',
-#         'USER': 'user42',
-#         'PASSWORD': 'user42',
-#         'HOST': 'db',
-#         'PORT': '5432',
-#     }
-# }
-
-# snemoto ローカル環境で開発用
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'transcendence_db',
+        'USER': 'user42',
+        'PASSWORD': 'user42',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
+
+# snemoto ローカル環境で開発用
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators

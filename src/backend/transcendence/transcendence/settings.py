2c0449eddfa9ec782ec917e565144a8df4296397
snemoto-42 <115632171+snemoto-42@users.noreply.github.com>
2024-04-06 19:07:39 +0900

    #17 settingの修正

--- src/backend/transcendence/transcendence/settings.py
+++ src/backend/transcendence/transcendence/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'game',
-	'game.apps.GameConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,17 +74,26 @@ WSGI_APPLICATION = 'transcendence.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'transcendence_db',
+#         'USER': 'user42',
+#         'PASSWORD': 'user42',
+#         'HOST': 'db',
+#         'PORT': '5432',
+#     }
+# }
+
+# snemoto
+# ローカル環境で開発するため
+# 後ほど削除
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'transcendence_db',
-        'USER': 'user42',
-        'PASSWORD': 'user42',
-        'HOST': 'db',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators

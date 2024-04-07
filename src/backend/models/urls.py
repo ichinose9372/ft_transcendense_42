06212cdaf1671b80f9f75203edf2799b8532d6e0
Yui Nishimura <nisshi.yui79@gmail.com>
2024-04-07 10:45:49 +0900

    /models /handlers /server /templates

--- src/backend/models/urls.py
+++ src/backend/models/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from game import views
+from handlers import views
 
 # 第１引数：HTTPリクエストのパス
 # 第２引数：ビュー関数
--- src/backend/server/urls.py
+++ src/backend/server/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from game.views import top
+from handlers.views import top
 
 # 第１引数：HTTPリクエストのパス
 # 第２引数：ビュー関数

371fa60e4fa6bcf39388795d54cd24dc24c28c3d
snemoto-42 <115632171+snemoto-42@users.noreply.github.com>
2024-04-09 19:03:27 +0900

    #31 for merge

--- src/backend/handlers/storeTournamentResult.py
+++ src/backend/handlers/storeTournamentResult.py
@@ -1,11 +1,11 @@
 # snemoto test for routing
-from django.shortcuts import render
-def storeTournamentResult(request):
-	return render(request, "storeTournamentResult.html")
+# from django.shortcuts import render
+# def storeTournamentResult(request):
+# 	return render(request, "storeTournamentResult.html")
 
-# from django.dispatch import receiver
-# from django.db.models.signals import post_save
-# from handlers.models import TournamentResult
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from handlers.models import TournamentResult
 
-# @receiver(post_save, sender=TournamentResult)
-# def handle_tournament_result(tournament, matches, scores):
+@receiver(post_save, sender=TournamentResult)
+def handle_tournament_result(tournament, matches, scores):
--- src/backend/server/settings.py
+++ src/backend/server/settings.py
@@ -77,24 +77,24 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'transcendence_db',
-#         'USER': 'user42',
-#         'PASSWORD': 'user42',
-#         'HOST': 'db',
-#         'PORT': '5432',
-#     }
-# }
-
-# snemoto ローカル環境で開発用
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'transcendence_db',
+        'USER': 'user42',
+        'PASSWORD': 'user42',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
+
+# snemoto ローカル環境で開発用
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators

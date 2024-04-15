ca99c2df06e514df4bfa353448445d4352ed8baa
snemoto-42 <115632171+snemoto-42@users.noreply.github.com>
2024-04-15 16:01:10 +0900

    #37 query

--- src/backend/handlers/getAchievements.py
+++ src/backend/handlers/getAchievements.py
@@ -1,4 +1,8 @@
 # snemoto test for routing
-# from django.shortcuts import render
+from django.shortcuts import render
 # def getAchievements(request):
 # 	return render(request, "getAchievements.html")
+
+def getAchievements(request):
+	participant_name = request.GET.get('participant_name', '')
+	return render(request, "getAchievements.html", {'participant_name': participant_name})
--- src/backend/models/tests.py
+++ src/backend/models/tests.py
@@ -33,11 +33,18 @@ class storeTournamentResultTest(TestCase):
 		response = self.client.get("/gamefinish/")
 		self.assertTemplateUsed(response, "storeTournamentResult.html")
 
-class getAchievementsTest(TestCase):
-	def test_returns_200_and_expected_title(self):
-		response = self.client.get("/dashboard/")
-		self.assertContains(response, "getAchievements", status_code=200)
+# class getAchievementsTest(TestCase):
+# 	def test_returns_200_and_expected_title(self):
+# 		response = self.client.get("/dashboard/")
+# 		self.assertContains(response, "getAchievements", status_code=200)
 
-	def test_uses_expected_template(self):
-		response = self.client.get("/dashboard/")
-		self.assertTemplateUsed(response, "getAchievements.html")
+# 	def test_uses_expected_template(self):
+# 		response = self.client.get("/dashboard/")
+# 		self.assertTemplateUsed(response, "getAchievements.html")
+
+class getAchievementsTest(TestCase):
+	def test_submit_participant_name(self):
+		participant_name = "Shota Nemoto"
+		response = self.client.get("/dashboard/", {'participant_name': participant_name})
+		self.assertContains(response, "getAchievements", status_code=200)
+		self.assertIn("Received participant: Shota Nemoto", response.content.decode())

9129ef56850cad4ed5f15b7cb8567bceaef386d6
ryhara <ryosei.hara@gmail.com>
2024-04-21 16:48:57 +0900

    TODO add

--- src/backend/handlers/getAchievements.py
+++ src/backend/handlers/getAchievements.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 
 def getAchievements(request):
     participant_name = request.GET.get('participant_name', '')
+    # TODO : 名前を元にDBからデータを取得してdataに格納，現状は名前だけを格納
     data = {
         'participant_name': participant_name
 	}

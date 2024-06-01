8d01e981b1972b97a606c2648f395dfb06e40bd8
yichinos <ichinose.0720.emt@gmail.com>
2024-06-01 11:19:50 +0900

    add : init-es.sh and init-container #55

---
+++ init-es.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Elasticsearchのヘルスチェックが成功するまで待機
+while ! curl -sSf "http://localhost:9200/" >/dev/null; do
+  echo "Waiting for Elasticsearch to be up..."
+  sleep 5
+done
+
+# ILMポリシーの作成
+curl -X PUT "http://localhost:9200/_ilm/policy/logstash-policy" -H 'Content-Type: application/json' -d'
+{
+  "policy": {
+    "phases": {
+      "hot": {
+        "actions": {
+          "rollover": {
+            "max_age": "30d",
+            "max_size": "50gb"
+          }
+        }
+      },
+      "delete": {
+        "min_age": "90d",
+        "actions": {
+          "delete": {}
+        }
+      }
+    }
+  }
+}
+'
+
+# 初期インデックスの作成
+curl -X PUT "http://localhost:9200/logstash-000001" -H 'Content-Type: application/json' -d'
+{
+  "aliases": {
+    "logstash-alias": {
+      "is_write_index": true
+    }
+  }
+}
+'
+
+# Elasticsearchをバックグラウンドで起動
+exec /usr/local/bin/docker-entrypoint.sh elasticsearch
